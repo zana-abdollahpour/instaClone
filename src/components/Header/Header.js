5a42b87da61085332acc2eedf2a63d7bc2e47721
zana-abdollahpour <zana.ap4@gmail.com>
2023-02-20 13:31:55 +0330

    make header sticky

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,10 +6,12 @@ import HeaderUser from "./HeaderUser";
 
 export default function Header() {
   return (
-    <div className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto">
-      <HeaderLogo />
-      <HeaderSearch />
-      <HeaderUser />
+    <div className="sticky top-0 z-30 bg-white border border-b shadow-md">
+      <div className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto">
+        <HeaderLogo />
+        <HeaderSearch />
+        <HeaderUser />
+      </div>
     </div>
   );
 }
