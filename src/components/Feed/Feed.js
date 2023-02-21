22b2427029cff9f74155132690c8c2393c2d0547
zana-abdollahpour <zana.ap4@gmail.com>
2023-02-21 18:13:06 +0330

    make Feed section responsive

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -7,12 +7,14 @@ import Posts from "./Posts/Posts";
 
 export default function Feed() {
   return (
-    <main>
-      <section>
+    <main className="grid grid-cols-1 md:grid-cols-3 md:max-w-6xl mx-auto">
+      <section className="md:col-span-2">
         <Stories />
         <Posts />
       </section>
-      <section>{/* Mini Profile + Suggestions */}</section>
+      <section className="hidden md:inline-grid md:col-span-1">
+        {/* Mini Profile + Suggestions */}
+      </section>
     </main>
   );
 }
