0f998db5365e2752ec7f8bba62990fa04c7dfa87
zana-abdollahpour <zana.ap4@gmail.com>
2023-02-22 11:27:47 +0330

    get the session and update header with google data

--- src/components/Header/HeaderUser.js
+++ src/components/Header/HeaderUser.js
@@ -2,19 +2,28 @@ import React from "react";
 import Image from "next/image";
 import { PlusCircleIcon } from "@heroicons/react/24/outline";
 import { HomeIcon } from "@heroicons/react/24/solid";
+import { useSession, signIn, signOut } from "next-auth/react";
 
-import userPhoto from "../../assets/img/users/user-zana.jpg";
+// import userPhoto from "../../assets/img/users/user-zana.jpg";
 
 export default function HeaderUser() {
+  const { data: session } = useSession();
   return (
     <div className="flex items-center space-x-4">
       <HomeIcon className="hidden h-6 transition-transform duration-200 ease-in md:inline-flex curpointer hover:scale-110" />
-      <PlusCircleIcon className="h-6 transition-transform duration-200 ease-in curpointer hover:scale-110" />
-      <Image
-        src={userPhoto}
-        alt="Zana's photo"
-        className="w-10 rounded-full cursor-pointer"
-      />
+      {session ? (
+        <>
+          <PlusCircleIcon className="h-6 transition-transform duration-200 ease-in curpointer hover:scale-110" />
+          <Image
+            onClick={signOut}
+            src={session.user.image}
+            alt="User's photo"
+            className="w-10 rounded-full cursor-pointer"
+          />
+        </>
+      ) : (
+        <button onClick={signIn}>Sign in</button>
+      )}
     </div>
   );
 }
