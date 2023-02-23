35b1736935fe6379a5a8be2453b0dc9c853ca884
zana-abdollahpour <zana.ap4@gmail.com>
2023-02-23 11:51:55 +0330

    add comment to the firebase DB

--- src/components/Feed/Posts/Post.js
+++ src/components/Feed/Posts/Post.js
@@ -1,5 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
-import React from "react";
+import React, { useState } from "react";
 import {
   EllipsisHorizontalIcon,
   HeartIcon,
@@ -7,21 +7,35 @@ import {
   BookmarkIcon,
   FaceSmileIcon,
 } from "@heroicons/react/24/outline";
-import Image from "next/image";
 import { useSession } from "next-auth/react";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 
-import userPhoto from "../../../assets/img/users/user-zana.jpg";
+// import userPhoto from "../../../assets/img/users/user-zana.jpg";
+import { db } from "../../../../firebase";
 
 export default function Post(props) {
   const { data: session } = useSession();
+  const [comment, setComment] = useState("");
+
+  async function sendComment(e) {
+    e.preventDefault();
+    const commentToSend = comment;
+    setComment("");
+    await addDoc(collection(db, "posts", props.id, "comments"), {
+      comment: commentToSend,
+      username: session.user.username,
+      userImage: session.user.image,
+      timestamp: serverTimestamp(),
+    });
+  }
 
   return (
     <div className="my-6 bg-white border rounded-md">
       {/* Post Header */}
       <div className="flex items-center p-5">
-        <Image
+        <img
           className="h-12 w-12 rounded-full object-cover brder p-[2px] mr-3"
-          src={userPhoto}
+          src={session.user.image}
           alt={props.username + "'s profile image"}
         />
         <span className="flex-1 block font-bold">{props.username}</span>
@@ -55,11 +69,20 @@ export default function Post(props) {
         <form className="flex items-center p-4">
           <FaceSmileIcon className="h-7" />
           <input
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
             className="flex-1 border-none focus:ring-0"
             type="text"
             placeholder="add your comment..."
           />
-          <button className="font-bold text-blue-400">Post</button>
+          <button
+            type="submit"
+            onClick={sendComment}
+            disabled={!comment.trim()}
+            className="font-bold text-blue-400 disabled:text-blue-200"
+          >
+            Post
+          </button>
         </form>
       )}
     </div>
